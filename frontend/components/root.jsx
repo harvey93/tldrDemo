f08ed82880898150615e24e84699432482d5f9f9
Hrant <Hrant@Hrants-MacBook-Air.local>
2017-04-07 09:41:28 -0700

    Initial commit

---
+++ frontend/components/root.jsx
@@ -0,0 +1,15 @@
+import { Router, Route, IndexRoute, hashHistory } from 'react-router';
+import App from './app';
+import React from 'react';
+import { Provider } from 'react-redux';
+
+
+const Root = ({ store }) => (
+  <Provider store={ store }>
+    <Router history={ hashHistory }>
+      <Route path="/" component={ App } />
+    </Router>
+  </Provider>
+);
+
+export default Root;
---
+++ frontend/reducers/test_reducer.js
@@ -0,0 +1,8 @@
+const TestReducer = (state = {}, action={type: 'test'}) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default TestReducer;
