30ced948e7853dd79bc60e3ef68482f5b24011b2
Hrant <Hrant@Hrants-MacBook-Air.local>
2017-04-07 12:17:21 -0700

    Add working icons

--- frontend/components/footer.jsx
+++ frontend/components/footer.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import FontAwesome from 'react-fontawesome';
 
 const Footer = ()=> {
   return(
     <div className='footer-div'>
-      <div>
+      <div className='profile-div'>
+
+        <div><i className="fa fa-github-square" aria-hidden="true"></i></div>
+        <div><i className="fa fa-linkedin-square" aria-hidden="true"></i></div>
         <h1>Harvey Mirijanyan</h1>
         <div>
           <a href='https://github.com/harvey93'>Github</a>
@@ -12,7 +16,7 @@ const Footer = ()=> {
           <a href='https://www.linkedin.com/in/harvey-mirijanyan-79a133ba'>Linkedin</a>
         </div>
       </div>
-      <div>
+      <div className='profile-div'>
         <h1>Ryley Sill</h1>
         <div>
           <a href='https://github.com/RyleySill93'>Github</a>
@@ -21,7 +25,7 @@ const Footer = ()=> {
           <a href='https://www.linkedin.com/in/ryleysill/'>Linkedin</a>
         </div>
       </div>
-      <div>
+      <div className='profile-div'>
         <h1>Daniel Cheng</h1>
         <div>
           <a href='https://github.com/MrDSGC'>Github</a>
