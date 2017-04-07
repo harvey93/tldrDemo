d40bc34674421b8ad69c6f1255e053ecdcdf532a
Hrant <Hrant@Hrants-MacBook-Air.local>
2017-04-07 11:26:59 -0700

    Add footer divs and basic styling

--- frontend/components/footer.jsx
+++ frontend/components/footer.jsx
@@ -2,21 +2,33 @@ import React from 'react';
 
 const Footer = ()=> {
   return(
-    <div>
+    <div className='footer-div'>
       <div>
         <h1>Harvey Mirijanyan</h1>
-        <h1>github</h1>
-        <h1>linkedin</h1>
+        <div>
+          <a href='https://github.com/harvey93'>Github</a>
+        </div>
+        <div>
+          <a href='https://www.linkedin.com/in/harvey-mirijanyan-79a133ba'>Linkedin</a>
+        </div>
       </div>
       <div>
         <h1>Ryley Sill</h1>
-        <h1>github</h1>
-        <h1>linkedin</h1>
+        <div>
+          <a href='https://github.com/RyleySill93'>Github</a>
+        </div>
+        <div>
+          <a href='https://www.linkedin.com/in/ryleysill/'>Linkedin</a>
+        </div>
       </div>
       <div>
         <h1>Daniel Cheng</h1>
-        <h1>github</h1>
-        <h1>linkedin</h1>
+        <div>
+          <a href='https://github.com/MrDSGC'>Github</a>
+        </div>
+        <div>
+          <a href='https://www.linkedin.com/in/dcheng47/'>Linkedin</a>
+        </div>
       </div>
     </div>
   );
