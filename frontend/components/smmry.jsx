b01dae2f1cbd899786a6f6d6efb1c0f88dd878d5
Hrant <Hrant@Hrants-MacBook-Air.local>
2017-04-08 16:58:19 -0700

    Add new icons

--- frontend/components/smmry.jsx
+++ frontend/components/smmry.jsx
@@ -8,19 +8,14 @@ class Smmry extends React.Component {
   render(){
     return(
       <div className='smmry-div'>
-        <div className='text-div'>
-          <h1 className='line'>Using the SMMRY Api,</h1>
-          <h1 className='line'>Tldr will summarize long form articles</h1>
-          <h1 className='line'>by 85 - 90%. No need to worry about the</h1>
-          <h1 className='line'>details, now you can just skim through the</h1>
-          <h1 className='line'>important parts of every article. From</h1>
-          <h1 className='line'>Sports and Entertainment to Politics and </h1>
-          <h1 className='line'>Business, all your news in one place!</h1>
+        <div>
+          <img src='http://res.cloudinary.com/harvey93/image/upload/c_scale,h_275/v1491694381/document-icon-19_g03xu6.png'/>
         </div>
-        <div className='image-div'>
-          <img
-            className='phone-image'
-            src='http://res.cloudinary.com/harvey93/image/upload/v1491693189/smmry-iphone_wrzrll.png' />
+        <div>
+          <img src='http://res.cloudinary.com/harvey93/image/upload/c_scale,h_200/v1491695103/arrow-point-to-right_tsyctn.png'/>
+        </div>
+        <div>
+          <img src='http://res.cloudinary.com/harvey93/image/upload/c_scale,h_200/v1491694380/document-icon-10_qfdaea.png'/>
         </div>
       </div>
     );
@@ -28,3 +23,20 @@ class Smmry extends React.Component {
 }
 
 export default Smmry;
+
+// <div className='smmry-div'>
+//   <div className='text-div'>
+//     <h1 className='line'>Using the SMMRY Api,</h1>
+//     <h1 className='line'>Tldr will summarize long form articles</h1>
+//     <h1 className='line'>by 85 - 90%. No need to worry about the</h1>
+//     <h1 className='line'>details, now you can just skim through the</h1>
+//     <h1 className='line'>important parts of every article. From</h1>
+//     <h1 className='line'>Sports and Entertainment to Politics and </h1>
+//     <h1 className='line'>Business, all your news in one place!</h1>
+//   </div>
+//   <div className='image-div'>
+//     <img
+//       className='phone-image'
+//       src='http://res.cloudinary.com/harvey93/image/upload/v1491693189/smmry-iphone_wrzrll.png' />
+//   </div>
+// </div>
