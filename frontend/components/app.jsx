7ef72b1564c44ab8f58b64ed27d7b8dd026494ce
Harvey Mirijanyan <harvey.mirijanyan@gmail.com>
2017-04-08 11:19:30 -0700

    Add Demo Skeleton

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Footer from './footer';
 import Header from './header';
+import Demo from './demo.jsx';
 
 const App = ({ children }) => (
   <div>
   <Header />
+  <Demo />
     { children }
     <Footer />
   </div>
---
+++ frontend/components/demo.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Demo extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render(){
+    return(
+      <div>
+        <h1>Demo</h1>
+      </div>
+    );
+  }
+}
+
+export default Demo;
