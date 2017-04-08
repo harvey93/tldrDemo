d0278234ae027e303f6c8eefe2a647a801b515d7
Hrant <Hrant@Hrants-MacBook-Air.local>
2017-04-08 15:37:22 -0700

    Add basic skeleton of smmry section

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Footer from './footer';
 import Header from './header';
-import Demo from './demo.jsx';
+import Demo from './demo';
+import Smmry from './smmry';
 
 const App = ({ children }) => (
   <div>
   <Header />
   <Demo />
+  <Smmry />
     { children }
     <Footer />
   </div>
