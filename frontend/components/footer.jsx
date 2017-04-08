a28325d4f2d04230e190c2a9caea6dea02c809b6
Harvey Mirijanyan <harvey.mirijanyan@gmail.com>
2017-04-07 19:57:49 -0700

    Change footer colors and styling

--- frontend/components/footer.jsx
+++ frontend/components/footer.jsx
@@ -13,50 +13,50 @@ class Footer extends React.Component {
       <div className='footer-div'>
         <div className='profile-div'>
 
-          <h1>Harvey Mirijanyan</h1>
+          <h1 className='name-tag'>Harvey Mirijanyan</h1>
 
           <a href='https://github.com/harvey93'>
             <div className='icon-div'>
               <i className="fa fa-github-square" aria-hidden="true"></i>
-              <h1>Github</h1>
+              <h1 className='link-text'>Github</h1>
             </div>
           </a>
 
           <a href='https://www.linkedin.com/in/harvey-mirijanyan-79a133ba'>
             <div className='icon-div'>
               <i className="fa fa-linkedin-square" aria-hidden="true"></i>
-              <h1>Linkedin</h1>
+              <h1 className='link-text'>Linkedin</h1>
             </div>
           </a>
         </div>
 
         <div className='profile-div'>
-          <h1>Ryley Sill</h1>
+          <h1 className='name-tag'>Ryley Sill</h1>
             <a href='https://github.com/RyleySill93'>
               <div className='icon-div'>
                 <i className="fa fa-github-square" aria-hidden="true"></i>
-                <h1>Github</h1>
+                <h1 className='link-text'>Github</h1>
               </div>
             </a>
             <a href='https://www.linkedin.com/in/ryleysill'>
               <div className='icon-div'>
                 <i className="fa fa-linkedin-square" aria-hidden="true"></i>
-                <h1>Linkedin</h1>
+                <h1 className='link-text'>Linkedin</h1>
               </div>
             </a>
         </div>
         <div className='profile-div'>
-          <h1>Daniel Cheng</h1>
+          <h1 className='name-tag'>Daniel Cheng</h1>
           <a href='https://github.com/MrDSGC'>
               <div className='icon-div'>
                 <i className="fa fa-github-square" aria-hidden="true"></i>
-                <h1>Github</h1>
+                <h1 className='link-text'>Github</h1>
               </div>
           </a>
           <a href='https://www.linkedin.com/in/dcheng47/'>
               <div className='icon-div'>
                 <i className="fa fa-linkedin-square" aria-hidden="true"></i>
-                <h1>Linkedin</h1>
+                <h1 className='link-text'>Linkedin</h1>
               </div>
           </a>
         </div>
