9e2ef84d8ca8c483cb955dd3c08006d10cba4a04
Harvey Mirijanyan <harvey.mirijanyan@gmail.com>
2017-04-09 16:28:12 -0700

    Change out tts logo

--- frontend/components/textToSpeech.jsx
+++ frontend/components/textToSpeech.jsx
@@ -9,8 +9,8 @@ class TextToSpeech extends React.Component {
     return(
       <div className='tts-div'>
         <div className='tts-text-div'>
-          <img className='tts-title-text' src='http://res.cloudinary.com/harvey93/image/upload/c_scale,w_300/v1491771718/Google_2015_logo.svg_rjsifs.png'/>
-          <p>With the Addition of Google Text To Speech, users now have the option to listen to article summaries. Perfect for the gym, listen to all the articles in your feed as you work out!</p>
+          <img className='tts-title-text' src='https://res.cloudinary.com/harvey93/image/upload/v1491780198/React_logo_wordmark_mvbish.png'/>
+          <p>With the Addition of React Native Text To Speech, users now have the option to listen to article summaries. Perfect for the gym, listen to all the articles in your feed as you work out!</p>
         </div>
         <div className='tts-img-div'>
           <img
@@ -23,3 +23,4 @@ class TextToSpeech extends React.Component {
 }
 
 export default TextToSpeech;
+// http://res.cloudinary.com/harvey93/image/upload/c_scale,w_300/v1491771718/Google_2015_logo.svg_rjsifs.png
