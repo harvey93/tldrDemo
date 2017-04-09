a7931065a8af405b0cd27cf9a61d6a1c9558e76d
Harvey Mirijanyan <harvey.mirijanyan@gmail.com>
2017-04-09 16:13:14 -0700

    Add fonts; fix opacity; change color scheme

--- frontend/components/demo.jsx
+++ frontend/components/demo.jsx
@@ -9,6 +9,11 @@ class Demo extends React.Component {
     return(
       <div className='demo-container'>
         <div className='demo-div'>
+          <div className='iphone-div'>
+          <img
+            className='gif'
+            src='https://res.cloudinary.com/harvey93/image/upload/v1491775737/giphy_ostf9l.gif'/>
+        </div>
         </div>
       </div>
     );
